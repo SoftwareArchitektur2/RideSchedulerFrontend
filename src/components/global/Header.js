2aff4e37aafe30d8d9d30b74b11d75445c6585bd
Jakob <j.wilmesmeier@gmail.com>
2022-03-08 14:01:51 +0100

    show selected tab

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,5 +1,5 @@
 import { Avatar, Grid, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Header.css';
 
@@ -13,13 +13,13 @@ export default function Header({menuData}) {
     return <>
     <div style={{'display': 'flex'}}>
         <Grid container spacing={0} className='header'>
-            <Grid item xs={4} className='headerField' onClick={() => navigate(menuData[0].link)}>
+            <Grid item xs={4} className={window.location.pathname == menuData[0].link ? 'headerFieldSelected' : 'headerField'} onClick={() => navigate(menuData[0].link)}>
                 <Typography variant='h6' className='menutext'>{menuData[0].text}</Typography>
             </Grid>
-            <Grid item xs={4} className='headerField' onClick={() => navigate(menuData[1].link)}>
+            <Grid item xs={4} className={window.location.pathname == menuData[1].link ? 'headerFieldSelected' : 'headerField'} onClick={() => navigate(menuData[1].link)}>
                 <Typography variant='h6' className='menutext'>{menuData[1].text}</Typography>
             </Grid>
-            <Grid item xs={4} className='headerField' onClick={() => navigate(menuData[2].link)}>
+            <Grid item xs={4} className={window.location.pathname == menuData[2].link ? 'headerFieldSelected' : 'headerField'} onClick={() => navigate(menuData[2].link)}>
                 <Typography variant='h6' className='menutext'>{menuData[2].text}</Typography>
             </Grid>
         </Grid>
